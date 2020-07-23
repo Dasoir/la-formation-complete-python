b5e045a13995166b9603db08c84b9ff950989cea
Dasoir <41457235+Dasoir@users.noreply.github.com>
2020-07-23 11:45:27 -0600

    Update script.py

--- chp-001_les-bases/recuperer-linput-de-lutilisateur/script.py
+++ chp-001_les-bases/recuperer-linput-de-lutilisateur/script.py
@@ -1,2 +1,7 @@
-nombre = input("Entrez un nombre: ")
-print(nombre)
+prenom = input("Entrez votre prenom: ")
+age = input ("Entrez votre âge:"
+ville = input("Entrez votre ville de naissance"
+             
+print(prenom)
+print(age)
+print(ville)
